226c53282ba52a90b7ddd3cb5a7bd2aa801b8371
ikmaal <ikmal528@gmail.com>
2020-02-09 00:52:53 +0800

    Integrated Feature 1, 2, 3, 4 and 5.
    1, 2, 3 and 4 works fine. However, there are still errors when i tried running 5 which is configuration

--- Maze.py
+++ Maze.py
@@ -35,16 +35,13 @@ def check_option(option, end=''):
         return "Option 1 selected"
 
     elif option == "2":
-        print("Option 2")
-        print("View Maze")
-        trace = viewMaze(maze)
-        print(trace)
+        viewMaze(maze)
     
     elif option == "3":
-        print("Option 3")
+        play_maze(maze)
     
     elif option == "4":
-        print("Option 4")
+        config_menu()
 
     else:
         print ("Invalid option")
@@ -65,6 +62,8 @@ def check_filename(filename):
         print("Error, file not found")
         return "Filename incorrect"
 
+####################################################################################
+
 def viewMaze(maze):
     trace = ""
     if 1 not in maze or exist == False:
@@ -74,6 +73,181 @@ def viewMaze(maze):
         trace = "Please load your maze first"
         
     return trace
+
+###################################################################################
+
+def play_maze(maze):
+    print('Option [3] Play maze game')
+    print('=================================')
+    
+    while True:
+        try:
+            for a in range(len(maze)):
+                for b in range(len(maze[a])):
+                    if maze[a][b]=='B':
+                        b_row=a
+                        b_col=b
+            for c in range(len(maze)):
+                for d in range(len(maze[c])):
+                    if maze[c][d]=='A':
+                        row = c
+                        col = d
+            for item in range(len(maze)):
+                print(maze[item])
+            print('Location of You(A) = (Row {}, Column {})'.format(row,col))
+            print('Location of End (B) = (Row {}, Column {})'.format(b_row,b_col))
+            movement=input("Press 'W' for UP, 'A' for LEFT, 'S' for DOWN, 'D' for RIGHT, 'M' for MAIN NENU': ")
+            movement = movement.upper()
+            if movement=='W':
+                if maze[row-1][col]=='X':
+                    print('Sorry, Wall Ahead. Please try another path')
+                else:
+                    maze[row][col]='O'
+                    maze[row-1][col]='A'
+                    print('UP successfully, press')
+            elif movement=='S':
+                if maze[row+1][col]=='X':
+                    print('Sorry, Wall Ahead. Please try another path')
+                else:
+                    maze[row][col]='O'
+                    maze[row+1][col]='A'
+                    print('DOWN successfully, press')
+            elif movement=='A':
+                if maze[row][col-1]=='X':
+                    print('Sorry, Wall Ahead. Please try another path')
+                else:
+                    maze[row][col]='O'
+                    maze[row][col-1]='A'
+                    print('LEFT successfully, press')
+            elif movement=='D':
+                if maze[row][col+1]=='X':
+                    print('Sorry, Wall Ahead. Please try another path')
+                else:
+                    maze[row][col]='O'
+                    maze[row][col+1]='A'
+                    print('RIGHT successfully, press')
+            elif movement =='M':
+                print ('Return to Menu')
+                break
+                display_menu()
+            else:   
+                print('Invalid Movement, try again!')
+        except 	IndexError:
+            print ('Oops! Out of Zone')
+            break
+            display_menu()
+
+#####################################################################################
+
+def config_menu():
+
+    print("Configuration Menu")
+    print("="*16)
+
+    options = 1
+    for i in configuration_menu:
+        print("[{0}] {1}".format(options, i))
+        options += 1
+    
+    print()
+    print("[0] Exit to Main Menu")
+    print()
+
+    config_options = input("Enter your options: ")
+    if( 0 <= int(config_options) < 5):
+        config_options = int(config_options)
+        options()
+    else:
+        print("Invalid input, please input a valid option from 0 to 4")
+    return config_options
+
+def options():
+    if config_options == 1:
+        config_create_wall()
+    elif config_options == 2:
+        config_create_passageway()
+    elif config_options == 3:
+        config_create_start()
+    elif config_options == 4:
+        config_create_end()
+
+def config_create_wall(input_row,input_col):
+    print("Enter the coordinate of the item you wish to change E.g. Row, Column")
+    print("'B' to return to Configure Menu")
+    input_row, input_col = input("'M' to return to Main Menu: ").split(",")
+
+    if (input_row.isdigit() and 0 <= int(input_row) < 9 and 0 <= int(input_col) < 9):
+        maze[(int(input_row)-1)][(int(input_col)-1)] = "X"
+
+        file = open("maze.csv","w")
+        for row in maze:
+            for col in row:
+                file.write("\n")
+    elif input_row == "B":
+        config_menu()
+    elif input_row == "M":
+        display_menu()
+    else:
+        print("Invalid input, please enter a valid input of either “B/M” or coordinate in the correct format")
+
+def config_create_passageway():
+    print("Enter the coordinate of the item you wish to change E.g. Row, Column")
+    print("'B' to return to Configure Menu")
+    input_row, input_col = input("'M' to return to Main Menu: ").split(",")
+
+    if (input_row.isdigit() and 0 <= int(input_row) < 9 and 0 <= int(input_col) < 9):
+        maze[(int(input_row)-1)][(int(input_col)-1)] = "O"
+
+        file = open("maze.csv","w")
+        for row in maze:
+            for col in row:
+                file.write("\n")
+    elif input_row == "B":
+        config_menu()
+    elif input_row == "M":
+        display_menu()
+    else:
+        print("Invalid input, please enter a valid input of either “B/M” or coordinate in the correct format")        
+
+def config_create_start():
+    print("Enter the coordinate of the item you wish to change E.g. Row, Column")
+    print("'B' to return to Configure Menu")
+    input_row, input_col = input("'M' to return to Main Menu: ").split(",")
+
+    if (input_row.isdigit() and 0 <= int(input_row) < 9 and 0 <= int(input_col) < 9):
+        maze[(int(input_row)-1)][(int(input_col)-1)] = "A"
+
+        file = open("maze.csv","w")
+        for row in maze:
+            for col in row:
+                file.write("\n")
+    elif input_row == "B":
+        config_menu()
+    elif input_row == "M":
+        display_menu()
+    else:
+        print("Invalid input, please enter a valid input of either “B/M” or coordinate in the correct format")
+
+def config_create_end():
+    print("Enter the coordinate of the item you wish to change E.g. Row, Column")
+    print("'B' to return to Configure Menu")
+    input_row, input_col = input("'M' to return to Main Menu: ").split(",")
+
+    if (input_row.isdigit() and 0 <= int(input_row) < 9 and 0 <= int(input_col) < 9):
+        maze[(int(input_row)-1)][(int(input_col)-1)] = "B"
+
+        file = open("maze.csv","w")
+        for row in maze:
+            for col in row:
+                file.write("\n")
+    elif input_row == "B":
+        config_menu()
+    elif input_row == "M":
+        display_menu()
+    else:
+        print("Invalid input, please enter a valid input of either “B/M” or coordinate in the correct format")
+
+
 
 if __name__ == "__main__":
     while run != False:
