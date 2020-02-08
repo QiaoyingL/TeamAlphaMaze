5bbeaafa679d88a07e235474b1cd2f2bb216807e
ikmaal <ikmal528@gmail.com>
2020-02-08 14:59:24 +0800

    Pytest (Read File)

---
+++ test_readfile.py
@@ -0,0 +1,16 @@
+import pytest
+from Maze import*
+import csv
+result = ""
+
+def test_filenameinput():
+    result = check_filename("Maze.csv")
+    assert result == "Filename correct"
+
+def test_filename_exact():
+    result = check_filename("Maze.csv")
+    assert result == "Filename correct"
+
+def test_filename_invalid():
+    result = check_filename("dssdfsdf")
+    assert result == "Filename incorrect"
